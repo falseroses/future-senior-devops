18fd3791fc41bb16c45180bf2403751326c478a1
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-06-15 00:01:33 +0300

    Project1 successfull

--- terraform/Project1/aws-vpc-create.tf
+++ terraform/Project1/aws-vpc-create.tf
@@ -6,42 +6,74 @@
 # Made by Oleksandr Dumynskyi
 
 
-provider "aws" {}
+provider "aws" {
+  region                  = "eu-central-1"
+  shared_credentials_file = "/home/alex/future-senior-devops/terraform/terraform-credentials"
+}
 
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "tf-example"
+    Name = "my_vpc"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "my_subnet1" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "eu-central-1a"
 
   tags = {
-    Name = "tf-example"
+    Name = "my_subnet1"
   }
 }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
+resource "aws_subnet" "my_subnet2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.16.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "my_subnet2"
+  }
+}
+
+resource "aws_network_interface" "number1" {
+  subnet_id   = aws_subnet.my_subnet1.id
   private_ips = ["172.16.10.100"]
 
   tags = {
-    Name = "primary_network_interface"
+    Name = "network_interface_1"
   }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0bad4a5e987bdebde" # us-west-2
+resource "aws_network_interface" "number2" {
+  subnet_id   = aws_subnet.my_subnet2.id
+  private_ips = ["172.16.16.100"]
+
+  tags = {
+    Name = "network_interface_2"
+  }
+}
+
+resource "aws_instance" "my_ami_1" {
+  ami           = "ami-0bad4a5e987bdebde"
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.foo.id
+    network_interface_id = aws_network_interface.number1.id
+    device_index         = 0
+  }
+}
+
+resource "aws_instance" "my_ami_2" {
+  ami           = "ami-0bad4a5e987bdebde"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.number2.id
     device_index         = 0
   }
 }
