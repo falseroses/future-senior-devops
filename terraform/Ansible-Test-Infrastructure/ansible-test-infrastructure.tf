fb1a4a3e0c93be2bb491825c75e2fad9e1a4118f
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-06-16 01:30:17 +0300

    1

--- terraform/Ansible-Test-Infrastructure/ansible-test-infrastructure.tf
+++ terraform/Ansible-Test-Infrastructure/ansible-test-infrastructure.tf
@@ -21,3 +21,41 @@ EOF
     Name = "Ansible-Master"
   }
 }
+
+resource "aws_instance" "Ansible-Master" {
+  ami = "ami-0bad4a5e987bdebde"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "sg-021b30133e9ed51f6",
+  ]
+  key_name      = "falseroses-key-Frankfurt"
+  user_data = <<EOF
+#!/bin/bash
+yum -y update
+echo "Starting install Ansible..."
+python3 -m pip install ansible
+echo "Done"
+EOF
+  tags = {
+    Name = "Ansible-Master"
+  }
+}
+
+resource "aws_instance" "Ansible-Master" {
+  ami = "ami-0bad4a5e987bdebde"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "sg-021b30133e9ed51f6",
+  ]
+  key_name      = "falseroses-key-Frankfurt"
+  user_data = <<EOF
+#!/bin/bash
+yum -y update
+echo "Starting install Ansible..."
+python3 -m pip install ansible
+echo "Done"
+EOF
+  tags = {
+    Name = "Ansible-Master"
+  }
+}
