56242a69d42e1c798cf868d1a79b8054d77f2dc2
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-06-16 01:16:01 +0300

    ansible-master server

---
+++ terraform/Ansible-Master/ansible-master-deploy.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region                  = "eu-central-1"
+  shared_credentials_file = "/home/alex/future-senior-devops/terraform/terraform-credentials"
+}
+
+resource "aws_instance" "Ansible-Master" {
+  ami = "ami-0bad4a5e987bdebde"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "sg-021b30133e9ed51f6",
+  ]
+  key_name      = "falseroses-key-Frankfurt"
+  user_data = <<EOF
+#!/bin/bash
+yum -y update
+echo "Starting install Ansible..."
+python3 -m pip install ansible
+echo "Done"
+EOF
+  tags = {
+    Name = "Ansible-Master"
+  }
+}
