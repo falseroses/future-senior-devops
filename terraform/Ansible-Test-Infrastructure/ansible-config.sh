e07c1c73db031e6304a692f4954c77cc863a148f
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-06-17 02:00:17 +0300

    some fixes

--- terraform/Ansible-Test-Infrastructure/ansible-config.sh
+++ terraform/Ansible-Test-Infrastructure/ansible-config.sh
@@ -5,8 +5,11 @@ python3 -m pip install ansible
 mkdir /home/ec2-user/ansible
 cat <<EOF > /home/ec2-user/ansible/hosts.txt
 [prod_servers]
-linux1 ansible_host=172.31.16.100 ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/.ssh/falseroses-key-Frankfurt.pem
-linux2 ansible_host=172.31.16.101 ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/.ssh/falseroses-key-Frankfurt.pem
+linux1 ansible_host=172.31.16.100
+linux2 ansible_host=172.31.16.101
+[prod_servers:vars]
+ansible_user=ec2-user
+ansible_ssh_private_key_file=/home/ec2-user/.ssh/falseroses-key-Frankfurt.pem
 EOF
 cat <<EOF > /home/ec2-user/ansible/ansible.cfg
 [defaults]
