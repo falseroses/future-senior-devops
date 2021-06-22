b5170ab8e8c1e0def113539826717910daa6e2de
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-06-22 22:28:05 +0300

    Adding new folder for example of deploy with Terraform Cloud

---
+++ terraform/Task-1-with-Terraform-Cloud/main.tf
@@ -0,0 +1,81 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "WebServer and Redis Security Group"
+  description = "WebServer and Redis Security Group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_webserv" {
+  count = 2
+  ami           = "ami-0bad4a5e987bdebde"
+  instance_type = "t2.micro"
+  user_data = file("install-nginx.sh")
+  subnet_id = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [
+    aws_security_group.my_security_group.id,
+  ]
+
+  tags = {
+    Name = "my_webserv"
+  }
+}
+
+resource "aws_instance" "my_redis_serv" {
+  ami           = "ami-0bad4a5e987bdebde"
+  instance_type = "t2.micro"
+  user_data = file("install-redis.sh")
+  subnet_id = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [
+    aws_security_group.my_security_group.id,
+  ]
+
+  tags = {
+    Name = "my_redis_serv"
+  }
+}
