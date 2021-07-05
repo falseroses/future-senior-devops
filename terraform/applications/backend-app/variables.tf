62d07684b2092786126ba13528d5b43336cb5a4a
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-07-05 14:38:22 +0300

    terraform cloud final setup

--- terraform/applications/backend-app/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to create things in."
-  type        = string
-}
-
-variable "aws_credentials" {
-  description = "The AWS credentials."
-  type        = string
-}
