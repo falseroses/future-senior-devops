3c5f778d6e40f58817760cb575a2ba36f18c2466
Oleksandr Dumynskyi <sasha.duminskiy@gmail.com>
2021-07-05 12:39:35 +0300

    complete db-redis server configuration

--- terraform/modules/db-redis/main.tf
+++ terraform/modules/db-redis/main.tf
@@ -7,17 +7,25 @@ provider "aws" {
 resource "aws_instance" "my_redis_serv" {
   ami           = "ami-0bad4a5e987bdebde"
   instance_type = "t2.micro"
+  key_name      = "falseroses-key-Frankfurt"
   subnet_id = data.aws_subnet.my_subnet.id
   vpc_security_group_ids = [data.aws_security_group.my_security_group.id]
-  user_data = <<EOF
-#!/bin/bash
-yum -y update
-echo "Starting installing Redis..."
-amazon-linux-extras install -y redis6
-systemctl start redis.service
-systemctl enable redis.service
-echo "All done!"
-EOF
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo amazon-linux-extras install -y redis6",
+      "sudo systemctl start redis.service",
+      "sudo systemctl enable redis.service",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("~/falseroses-key-Frankfurt.pem")
+  }
 
   tags = {
     Name = "my_redis_serv"
